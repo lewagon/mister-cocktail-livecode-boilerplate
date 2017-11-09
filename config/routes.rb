832c8397d8012cd149419398b768a0785a4c1a5b
Sébastien Saunier <seb@saunier.me>
2017-11-09 19:28:29 +0000

    Routes were weirdly defined.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
-  Rails.application.routes.draw do
-    resources :cocktails, only: [:show, :index, :new, :create]
-    root "cocktails#index"
-  end
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :cocktails, only: [:show, :index, :new, :create]
+  root "cocktails#index"
 end
